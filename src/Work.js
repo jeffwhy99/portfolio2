96bd5f4592697d8cb71305b1ceacfe951bb06e9f
System Administrator <root@Jeffreys-MacBook-Pro.local>
2020-12-13 04:24:43 -0800

    update foodie

--- src/Work.js
+++ src/Work.js
@@ -8,7 +8,7 @@ function Work(props) {
         return (
             <div>
                     <Link to="/work/foodie">
-                        <Card name="Foodie" desc="This section is incomplete. Will update on 12/12." photo={Thumbnail} />
+                        <Card name="Foodie" desc="Designing the IA and UI for a ficticious app that serves as a one-stop resource for all things food-related" photo={Thumbnail} />
                     </Link>
                     {/* <Link to="/work/securate">
                         <Card name="Securate" desc="I designed an app that determines the how intrusive other apps are." photo={Thumbnail} />
