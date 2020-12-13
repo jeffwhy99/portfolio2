c900775b374c81c6fc780e5fb0f82bb255a9c5dd
System Administrator <root@Jeffreys-MacBook-Pro.local>
2020-12-12 21:26:12 -0800

    Updates

---
+++ precache-manifest.bd6250a5736fc898ab8d9c15a1c45739.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc5beca7cb10c5204dc4030842d38c3b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "31e268b38054a4900535",
+    "url": "/portfolio/static/css/main.daa09f77.chunk.css"
+  },
+  {
+    "revision": "f62bcd67edd26ee01a72",
+    "url": "/portfolio/static/js/2.851014bc.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/portfolio/static/js/2.851014bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31e268b38054a4900535",
+    "url": "/portfolio/static/js/main.68ddda45.chunk.js"
+  },
+  {
+    "revision": "b37b5bb5a98282a2f0e7",
+    "url": "/portfolio/static/js/runtime-main.b449097c.js"
+  },
+  {
+    "revision": "425dca41d1f3b8a4b896df28309c9c24",
+    "url": "/portfolio/static/media/mark_novo_ss.425dca41.png"
+  },
+  {
+    "revision": "ded4d7a1611124551a7c9bbedaf6ab0d",
+    "url": "/portfolio/static/media/sapporo_xmas.ded4d7a1.jpg"
+  },
+  {
+    "revision": "9aa76543db80dbeab5d386448316a245",
+    "url": "/portfolio/static/media/sapporo_xmas_tn.9aa76543.jpg"
+  },
+  {
+    "revision": "14349c9b8f9967b166dc880e16bbc450",
+    "url": "/portfolio/static/media/thumbnail_template.14349c9b.jpg"
+  },
+  {
+    "revision": "3c692e28c14da10abeae9361e7f88810",
+    "url": "/portfolio/static/media/work_ss.3c692e28.png"
+  }
+]);
