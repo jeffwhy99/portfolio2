440f070ca62cf215fd982538796dbdf078e9e8f6
System Administrator <youngjeff21w@gmail.com>
2021-02-26 05:57:07 -0800

    updates

---
+++ build/precache-manifest.6e1ebc9e447528386400fec7e3e218a2.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f21c9be433302b8e0b1fcad9ba3e6070",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c1d015a83c6b1ecfa0f8",
+    "url": "/portfolio/static/css/main.c790c5d6.chunk.css"
+  },
+  {
+    "revision": "17840281fe08dc2b86a2",
+    "url": "/portfolio/static/js/2.f0845e5b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/portfolio/static/js/2.f0845e5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1d015a83c6b1ecfa0f8",
+    "url": "/portfolio/static/js/main.26240210.chunk.js"
+  },
+  {
+    "revision": "b37b5bb5a98282a2f0e7",
+    "url": "/portfolio/static/js/runtime-main.b449097c.js"
+  },
+  {
+    "revision": "0668569864b6d5eb50084f5294bd3231",
+    "url": "/portfolio/static/media/about_figma.06685698.png"
+  },
+  {
+    "revision": "8883056ec5f070a569427ea9275b6dee",
+    "url": "/portfolio/static/media/account_ia.8883056e.png"
+  },
+  {
+    "revision": "4acc4bb4ba9734043028fb39b967d28b",
+    "url": "/portfolio/static/media/blue_flower.4acc4bb4.jpg"
+  },
+  {
+    "revision": "296cf9750153aec7051ef5daa58b8d08",
+    "url": "/portfolio/static/media/blue_pic.296cf975.jpg"
+  },
+  {
+    "revision": "6a88b515c0da3b5bda5c8ee42e30d889",
+    "url": "/portfolio/static/media/blue_portrait.6a88b515.jpg"
+  },
+  {
+    "revision": "ac77b33d40ea3885025bd6edad73b158",
+    "url": "/portfolio/static/media/features_known.ac77b33d.png"
+  },
+  {
+    "revision": "193f8e4bf5e47bee11a2e84cdb7f0545",
+    "url": "/portfolio/static/media/foodie_card_sort.193f8e4b.png"
+  },
+  {
+    "revision": "efb805bb5a784b533d9fa3495785cdb1",
+    "url": "/portfolio/static/media/foodie_figma.efb805bb.png"
+  },
+  {
+    "revision": "376b73c4175953c50fc0d35445a49580",
+    "url": "/portfolio/static/media/foodie_persona.376b73c4.jpg"
+  },
+  {
+    "revision": "5fa42e1f2c6c7eeda31f33941bac8ea0",
+    "url": "/portfolio/static/media/hours_daily.5fa42e1f.png"
+  },
+  {
+    "revision": "14dbbea496e6c45af95d9b79a921ce19",
+    "url": "/portfolio/static/media/mark_novo_ss.14dbbea4.png"
+  },
+  {
+    "revision": "3948fa6cc528dc9ed96a0ea341309c21",
+    "url": "/portfolio/static/media/proj_figma.3948fa6c.png"
+  },
+  {
+    "revision": "7afb3b08a2f312edc631161cede859e4",
+    "url": "/portfolio/static/media/recipe_ia.7afb3b08.png"
+  },
+  {
+    "revision": "637973f9a3cabb28a8a2d5d3d9b94612",
+    "url": "/portfolio/static/media/sushi.637973f9.jpg"
+  },
+  {
+    "revision": "bc08ff29e425d8e859681e7238565fec",
+    "url": "/portfolio/static/media/sushi_tn.bc08ff29.jpg"
+  },
+  {
+    "revision": "14349c9b8f9967b166dc880e16bbc450",
+    "url": "/portfolio/static/media/thumbnail_template.14349c9b.jpg"
+  },
+  {
+    "revision": "1d96f357ab4edcb878fe45ca7b4f8df4",
+    "url": "/portfolio/static/media/work_figma.1d96f357.png"
+  }
+]);
